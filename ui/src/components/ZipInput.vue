bf2b602e913fe8673d7fb6fa0522226d08c99362
tyren1995 <tyrenrhinehart@yahoo.com>
2022-11-10 18:29:17 -0500

    remove unnecessary files, create super ugly zip input

---
+++ ui/src/components/ZipInput.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      zip: null
+    }
+  }
+}
+</script>
+
+<template>
+  <form>
+  <input v-model="zip" class="zip-searcher" />
+  </form>
+</template>
+
+<style>
+.zip-searcher {
+  color: red;
+  font-weight: bold;
+}
+</style>
