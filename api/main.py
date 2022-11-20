eb15471230d5b19803d96ba2f32633466fc97c8d
tyren1995 <tyrenrhinehart@yahoo.com>
2022-11-20 18:56:06 -0500

    install and use xmlToDict, return only the city and state in response

--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
-from typing import Union
-
 from fastapi import FastAPI
 import requests
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 from pydantic import BaseModel
+
+import xmltodict
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,8 +23,17 @@ url = "https://secure.shippingapis.com/ShippingAPI.dll?API=CityStateLookup"
 
 @app.post("/find-zip")
 async def get_city_state(zip:Zip):
+
     zip_dict = zip.dict()
     zip_value = zip_dict['zip']
     xml = f'<CityStateLookupRequest USERID="436TYREN7317"><ZipCode ID= "0"><Zip5>{zip_value}</Zip5></ZipCode></CityStateLookupRequest>'
     response = requests.get(f'{url}&xml={xml}')
-    return response.content
+    content = xmltodict.parse(response.content)
+    print(content)
+    city = content['CityStateLookupResponse']['ZipCode']['City']
+    state = content['CityStateLookupResponse']['ZipCode']['State']
+    formatted_response = {
+        "city" : city,
+        "state" : state
+    }
+    return formatted_response
--- ui/src/components/ZipInput.vue
+++ ui/src/components/ZipInput.vue
@@ -3,8 +3,16 @@ import axios from 'axios'
 import { ref } from 'vue'
 
 const zip = ref('');
+let cityAndState = ref('');
+let displayCityAndState = ref(false);
+
 const search = () => (
-axios.post('http://127.0.0.1:8000/find-zip', {"zip" : zip.value})
+axios.post('http://127.0.0.1:8000/find-zip', {"zip" : zip.value}).then(
+  (res)=>(
+      cityAndState.value = res,
+      displayCityAndState.value = true
+  )
+)
 );
 
 </script>
@@ -13,6 +21,7 @@ axios.post('http://127.0.0.1:8000/find-zip', {"zip" : zip.value})
   <h1>Zip Finder</h1><br/>
   <input v-model="zip" class="zip-searcher" />
   <button @click="search">Search</button>
+  <p v-if="displayCityAndState">{{cityAndState}}</p>
 </template>
 
 <style>
