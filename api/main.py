3058f562bee4fea29ca9905d63bcbe8a33132e4e
tyren1995 <tyrenrhinehart@yahoo.com>
2022-11-20 16:06:36 -0500

    leverage python requests lib, vue refs,axios to send and receive data, properly handle the xml response

--- api/main.py
+++ api/main.py
@@ -2,12 +2,29 @@ from typing import Union
 
 from fastapi import FastAPI
 import requests
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+from pydantic import BaseModel
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Zip(BaseModel):
+    zip: str
 
 url = "https://secure.shippingapis.com/ShippingAPI.dll?API=CityStateLookup"
 
-@app.get("/find-zip")
-def get_city_state(xml: str):
+
+@app.post("/find-zip")
+async def get_city_state(zip:Zip):
+    zip_dict = zip.dict()
+    zip_value = zip_dict['zip']
+    xml = f'<CityStateLookupRequest USERID="436TYREN7317"><ZipCode ID= "0"><Zip5>{zip_value}</Zip5></ZipCode></CityStateLookupRequest>'
     response = requests.get(f'{url}&xml={xml}')
-    return response
+    return response.content
--- ui/src/components/ZipInput.vue
+++ ui/src/components/ZipInput.vue
@@ -1,13 +1,18 @@
-<script>
+<script setup>
 import axios from 'axios'
+import { ref } from 'vue'
+
+const zip = ref('');
+const search = () => (
+axios.post('http://127.0.0.1:8000/find-zip', {"zip" : zip.value})
+);
+
 </script>
 
 <template>
   <h1>Zip Finder</h1><br/>
-  <form action="http://127.0.0.1:8000/find-zip">
   <input v-model="zip" class="zip-searcher" />
-  <button id="submit-button" type="submit">Search</button>
-  </form>
+  <button @click="search">Search</button>
 </template>
 
 <style>
