50098ce6000b2e7fe32752d2e5037872e5abf10f
tyren1995 <tyrenrhinehart@yahoo.com>
2022-11-20 20:03:20 -0500

    use env variable for usps userid

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ import requests
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import xmltodict
+import os
 
 app = FastAPI()
 
@@ -23,13 +24,16 @@ async def get_city_state(zip:Zip):
     url = "https://secure.shippingapis.com/ShippingAPI.dll?API=CityStateLookup"
     zip_dict = zip.dict()
     zip_value = zip_dict['zip']
-    xml = f'<CityStateLookupRequest USERID="436TYREN7317"><ZipCode ID= "0"><Zip5>{zip_value}</Zip5></ZipCode></CityStateLookupRequest>'
+    xml = f'<CityStateLookupRequest USERID="{os.environ["USERID"]}"><ZipCode ID= "0"><Zip5>{zip_value}</Zip5></ZipCode></CityStateLookupRequest>'
     response = requests.get(f'{url}&xml={xml}')
-    responseDict = xmltodict.parse(response.content)
-    city = responseDict['CityStateLookupResponse']['ZipCode']['City']
-    state = responseDict['CityStateLookupResponse']['ZipCode']['State']
-    formatted_response = {
-        "city" : city,
-        "state" : state
-    }
-    return formatted_response
+    if response.status_code == 200:
+        responseDict = xmltodict.parse(response.content)
+        city = responseDict['CityStateLookupResponse']['ZipCode']['City']
+        state = responseDict['CityStateLookupResponse']['ZipCode']['State']
+        formatted_response = {
+            "city" : city,
+            "state" : state
+        }
+        return formatted_response
+    else:
+        return "sorry there has been an error"
