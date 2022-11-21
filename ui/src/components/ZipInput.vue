8689a6d567d988f55203ddbb54b2c988c3866aed
tyren1995 <tyrenrhinehart@yahoo.com>
2022-11-20 19:42:52 -0500

    give search function a better name, clean up ZipInput file formatting

--- ui/src/components/ZipInput.vue
+++ ui/src/components/ZipInput.vue
@@ -7,21 +7,18 @@ let city= ref('');
 let state= ref('');
 let displayCityAndState = ref(false);
 
-const search = () => {
+const queryUSPSAPI = () => {
 if(zip.value.length !== 5){
  return alert('Please enter a valid 5 digit zip code.')
 }
 else{
-return axios.post('http://127.0.0.1:8000/find-zip', {"zip" : zip.value}).then(
-  (res)=>(
+return axios.post('http://127.0.0.1:8000/find-zip', {"zip" : zip.value}).then((res)=>(
       city.value = res.data['city'],
       state.value = res.data['state'],
       displayCityAndState.value = true
-  )
-)
+  ))
 }
 };
-
 </script>
 
 <template>
@@ -29,9 +26,11 @@ return axios.post('http://127.0.0.1:8000/find-zip', {"zip" : zip.value}).then(
   <h1>Zip Finder</h1><br/>
   <div>
   <input v-model="zip" id="zip-searcher" />
-  <button id="search-button" @click="search">Search</button>
+  <button id="search-button" @click="queryUSPSAPI">Search</button>
   </div>
-  <h1 id="response-message" v-if="displayCityAndState"> The city and state of the entered zip is <br/><span id="location">{{city}},{{state}}</span> </h1>
+  <h1 id="response-message" v-if="displayCityAndState"> 
+    The city and state of the entered zip is <br/><span id="location">{{city}},{{state}}</span> 
+  </h1>
   </div>
 </template>
 
@@ -46,7 +45,6 @@ h1, button{
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
   background-color: #6EC1E4;
   padding:70px;
   border-radius: 3px;
